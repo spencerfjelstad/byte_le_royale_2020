1b92a78fa4ee0e64c5db51ecd60414fec0057261
sirdude103 <sirdude103@gmail.com>
2019-05-02 15:18:25 -0500

    added stats file and example enums

--- game/common/enums.py
+++ game/common/enums.py
@@ -1,5 +1,4 @@
 
-
 class DisasterType:
     fire = 0
     tornado = 1
@@ -7,3 +6,18 @@ class DisasterType:
     earthquake = 3
     monster = 4
     ufo = 5
+
+class ActionType:
+    none = 0
+    repair = 1
+    upgrade = 2
+    accumulate_wealth = 3
+    build = 4
+
+class SensorType:
+    fire_alarm = 0
+    rock_on_a_rope = 1
+    coast_guard = 2
+    seismograph = 3
+    ## TODO: come up with a sensor that detects monsters
+    satellite_dish = 5
---
+++ game/common/stats.py
@@ -0,0 +1,18 @@
+from game.common.enums import *
+
+class GameStats:
+
+    # units of man_power per 1k population
+    man_power = 5
+
+    # cost in man_power to build a sensor
+    sensor_cost_0 = 20
+    sensor_cost_1 = 50
+    sensor_cost_2 = 100
+    sensor_cost_3 = 500
+
+    # error range provided by each sensor
+    sensor_range_0 = 30
+    sensor_range_1 = 15
+    sensor_range_2 = 8
+    sensor_range_3 = 2
