5df2ac62848442d4b40c61e62165a148b6850dc5
PixPanz <fjelstad1678@gmail.com>
2019-05-29 05:00:53 -0500

    Added Tornado class

--- game/common/disasters/__init__.py
+++ game/common/disasters/__init__.py
@@ -1,10 +1,12 @@
 from game.common.disasters.disaster import Disaster
 from game.common.disasters.lasting_disaster import LastingDisaster
 from game.common.disasters.fire import Fire
+from game.common.disasters.tornado import Tornado
 
 
 __all__ = [
     'Disaster',
     'LastingDisaster',
-    'Fire'
+    'Fire',
+    'Tornado'
 ]
--- game/tests/test_disaster_objects.py
+++ game/tests/test_disaster_objects.py
@@ -4,17 +4,27 @@ from game.common.disasters import *
 
 class TestDisasterObjects(unittest.TestCase):
 
-    def test_init(self):
+    #  Fire Object Testing
+
+    def test_init_fire(self):
         my_fire = Fire()
         self.assertIsInstance(my_fire, Fire)
         self.assertIsInstance(my_fire, LastingDisaster)
         self.assertIsInstance(my_fire, Disaster)
 
-    def test_abstract_init_fail(self):
+    def test_abstract_init_fire_fail(self):
         self.assertRaises(TypeError, lambda: LastingDisaster())
 
-    def test_abstract_init_fail_2(self):
+    def test_abstract_init_fire_fail_2(self):
         self.assertRaises(TypeError, lambda: Disaster())
+
+    
+    #  Tornado Object Testing
+
+    #def test_init_tornado(self):
+    #    my_tornado = Tornado()
+    #    self.assertIsInstance(my_tornado, Tornado)
+    #    self.assertIsInstance(my_tornado, Disaster)
 
 
 if __name__ == '__main__':
