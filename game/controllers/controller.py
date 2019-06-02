077962b311ab3cc1648310ab76d2e1ef052e0c2b
sirdude103 <sirdude103@gmail.com>
2019-06-01 22:41:03 -0500

    Removed singleton structure. Added debug

--- game/controllers/controller.py
+++ game/controllers/controller.py
@@ -1,24 +1,10 @@
 from abc import ABC, abstractmethod
-import warnings
 
 
 class Controller(ABC):
-    __instance = None
-
-    @abstractmethod
-    def __init__(self):
-        if self.__class__.__instance is None:
-            self.__class__.__instance = self
-        else:
-            warnings.warn("There is already an instance of " + self.__class__.__name__ + ". "
-                          "Use " + self.__class__.__name__ + ".get_instance() to get an instance of this class.")
+    debug = False
 
     @classmethod
-    def get_instance(cls):
-        if cls.__instance is None:
-            cls()
-        return cls.__instance
-
-    @classmethod
-    def del_instance(cls):
-        cls.__instance = None
+    def log(cls, message):
+        if cls.debug is True:
+            print(message)
--- game/controllers/economy_controller.py
+++ game/controllers/economy_controller.py
@@ -2,6 +2,4 @@ from game.controllers import Controller
 
 
 class EconomyController(Controller):
-
-    def __init__(self):
-        super().__init__()
+    pass
