60e5f98dce3704df3bbdd83f87e61effbeccf818
sirdude103 <sirdude103@gmail.com>
2019-06-06 22:16:26 -0500

    Really poor quality pause after batch files

--- generate.bat
+++ generate.bat
@@ -1,1 +1,16 @@
 python -m game.scripts.generate
+
+if /I "%1"=="true" GOTO P
+if /I "%1"=="t" GOTO P
+if /I "%1"=="pause" GOTO P
+if /I "%1"=="p" GOTO P
+if /I "%1"=="debug" GOTO P
+if /I "%1"=="d" GOTO P
+
+GOTO END
+
+:P 
+
+pause
+
+:END
