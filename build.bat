e9132a83178d91ce7c49acf6c8514a678dc47686
topoftheyear <topoftheyear@Gmail.com>
2019-06-28 18:38:33 -0500

    Reworked system to build to compressed .pyz file. Further functionality and arguments are extendable. Closes #49

---
+++ build.bat
@@ -0,0 +1,5 @@
+del /q *.pyz
+
+xcopy /s/e/i "game" "wrapper/game"
+python -m zipapp "wrapper" -o "launcher.pyz"
+del /q/s "wrapper/game"
---
+++ wrapper/__main__.py
@@ -0,0 +1,45 @@
+import sys
+
+from game.utils.generate_game import generate
+from game.main import loop
+from game.config import *
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('No arguments given. Append "-help" for a list of them.')
+        exit()
+    
+    # Generate game options
+    if '-generate' in sys.argv:
+        generate()
+    
+    # Run game options
+    elif '-run' in sys.argv:
+        loop()
+    
+    # Visualizer options
+    elif '-visualizer' in sys.argv:
+        # importing in the middle of a file is not good practice but its done here to quiet pygame for non-visualizer options
+        from game.visualizer import start
+        
+        # Additional args
+        full = False
+        gamma = GAMMA
+        if '-fullscreen' in sys.argv:
+           full = True 
+        if '-gamma' in sys.argv:
+            gamma_in = sys.argv.index('-gamma') + 1
+            if len(sys.argv) > gamma_in:
+                gamma = float(sys.argv[gamma_in])
+            else:
+                print('Gamma input not found, using default value')
+                
+            print(gamma)
+        start(gamma, True)
+        
+    # Help
+    elif '-help' in sys.argv:
+        print("Here's a list of arguments:")
+        print("-generate")
+        print("-run")
+        print("-visualizer  |  subarguments:[-fullscreen, -gamma]")
