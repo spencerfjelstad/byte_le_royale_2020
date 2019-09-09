66b7d0d4c5d5bd829f4a978ffbe32a4c735abfb8
nicholas-pfeifer <nicholas.pfeifer@ndsu.edu>
2019-09-09 15:13:50 -0500

    Modularized visualizer code into graphs, init, and log parser

--- game/visualizer/__init__.py
+++ game/visualizer/__init__.py
@@ -8,6 +8,7 @@ import pygame
 from pygame.locals import *
 
 from game.visualizer.game_log_parser import GameLogParser
+from game.visualizer.graphs import *
 from game.config import *
 
 pause = False
@@ -84,7 +85,7 @@ def draw_screen(current_turn):
     if turn_info is None:
         endgame()
     turn_indicator = font.render(f'Turn {turn}', True, (150, 140, 130))
-    health_bar(turn_info)
+    health_bar(turn_info, global_surf)
     global_surf.blit(turn_indicator, (30, 500))
     n = 0
     for key, item in turn_info['rates'].items():
@@ -100,47 +101,13 @@ def endgame():
     textsurface = font.render('Some Text', False, (0, 0, 0))
     global_surf.blit(textsurface,(0,0))
     # Draws graph of round's data
-    lineGraph()
+    lineGraph(global_surf)
 
     pygame.display.update()
 
     #Stays running till exit button pressed
     while True:
         handle_events()
-        
-def health_bar(info):
-    structure = int(info['player'].get('city').get('structure'))
-    population = int(info['player'].get('city').get('population'))
-    struct = pygame.Rect(100, 600, structure//10, 50)
-    pop = pygame.Rect(100,550,population//10,50)
-    pygame.draw.rect(global_surf, pygame.Color(255,0,0), struct)
-    pygame.draw.rect(global_surf, pygame.Color(255,255,0), pop)
-
-
-def lineGraph():
-    #For now, here's how to draw a basic graph with random points
-    #Game results, to be gotten from logs
-    max_population = 100
-    max_turns = 200
-    #Border
-    #Variables and rectangle declaration
-    x_coord = 100
-    y_coord = 100
-    width = 250
-    length = 200
-    border = pygame.Rect(x_coord,y_coord,width,length)
-    inner_border = pygame.Rect(x_coord + 2, y_coord + 2, width-4, length-4)
-    #Draw rectangles
-    pygame.draw.rect(global_surf, pygame.Color(0,0,0), border)
-    pygame.draw.rect(global_surf, pygame.Color(0,255,255), inner_border)
-
-    #Ticks
-    #Num of ticks to be a constant 10, the turn and pop values divided to fit
-    for i in range(0,9):
-        pygame.draw.line(global_surf, pygame.Color(0,0,0), (x_coord + (width*(i+1)/10), y_coord + length -1), (x_coord + (width*(i+1)/10), y_coord + length + 1))
-
-
-    #global_surf.draw.rect
 
 
 def handle_events():
