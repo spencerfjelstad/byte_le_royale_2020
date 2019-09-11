bdf0487cc12c28f86174ab8e268953dbad18fb32
Sophie <sophie.noel@ndsu.edu>
2019-09-11 11:47:37 -0500

    Basic functionality for line graph.

--- game/visualizer/graphs.py
+++ game/visualizer/graphs.py
@@ -18,7 +18,7 @@ def lineGraph(surf):
     #Variables and rectangle declaration
     x_coord = 100
     y_coord = 100
-    width = 250
+    width = 400
     length = 200
     border = pygame.Rect(x_coord,y_coord,width,length)
     inner_border = pygame.Rect(x_coord + 2, y_coord + 2, width-4, length-4)
@@ -29,7 +29,19 @@ def lineGraph(surf):
     #Ticks
     #Num of ticks to be a constant 10, the turn and pop values divided to fit
     for i in range(0,9):
-        pygame.draw.line(surf, pygame.Color(0,0,0), (x_coord + (width*(i+1)/10), y_coord + length -1), (x_coord + (width*(i+1)/10), y_coord + length + 1))
+        #x-axis ticks
+        pygame.draw.line(surf, pygame.Color(0,0,0), (x_coord + (width*(i+1)/10), y_coord + length), (x_coord + (width*(i+1)/10), y_coord + length - 4))
+        #y-axis ticks
+        pygame.draw.line(surf, pygame.Color(0,0,0), (x_coord, y_coord + (length*(i+1)/10)), (x_coord + 4, y_coord + (length*(i+1)/10)))
 
+    #Draw lines in graph
+    x_origin = x_coord
+    y_origin = y_coord + length
+    #Test values for y value
+    values = [50,67,10,100,79,56,24,79,35,78,36,35,78,17,96,46,86,23,67,56]
+    for i in range(0,19):
+        pygame.draw.line(surf, pygame.Color(0,0,0), (x_origin,y_origin),(x_coord + (20*(i+1)), y_coord + length - values[i]))
+        x_origin = x_coord + (20*(i+1))
+        y_origin = y_coord + length - values[i]
 
     #surf.draw.rect
