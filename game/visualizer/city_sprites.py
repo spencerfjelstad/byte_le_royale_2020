37b4a550abb4c04ea6fce1ef1be7a8d4f4bfefd3
terrainbuilder52 <32080764+terrainbuilder52@users.noreply.github.com>
2019-09-26 17:56:58 -0500

    Added the rest of the city

--- game/visualizer/city_sprites.py
+++ game/visualizer/city_sprites.py
@@ -5,7 +5,7 @@ from game.visualizer.game_log_parser import *
 
 def draw_city_sprites(turn_info, surf):
     x = 484
-    y = 200
+    y = 0
     #Draw Sprite object
     CitySprite(x , y , surf, turn_info)
 
@@ -17,20 +17,31 @@ class CitySprite(pygame.sprite.Sprite):
         super().__init__()
         self.info = turn_info
         images = [
-            "game/visualizer/assets/city_assets/city_level1.png",
             "game/visualizer/assets/city_assets/city_default.png",
+            "game/visualizer/assets/city_assets/city_level1.png",
+            "game/visualizer/assets/city_assets/city_level2.png",
+            "game/visualizer/assets/city_assets/city_level3.png"
         ]
+        structure = 0
+        try:
+            structure = int(self.info['player'].get('city').get('structure'))
+        except:
+            print("NO! >:(")
 
-        structure = int(self.info['player'].get('city').get('structure'))
-        if structure is not None:
-            if structure > 150:
-                self.image = pygame.image.load(images[0])
-            else:
-                self.image = pygame.image.load(images[1])
+        if structure > 150:
+            self.image = pygame.image.load(images[3])
+        elif structure > 100:
+            self.image = pygame.image.load(images[2])
+        elif structure > 50:
+            self.image = pygame.image.load(images[1])
+        else:
+            self.image = pygame.image.load(images[0])
 
-        surf.blit(self.image, (x,y))
         #image needs rect to work
         self.rect = self.image.get_rect()
+
+        surf.blit(self.image, (x,y))
+
 
 
 
