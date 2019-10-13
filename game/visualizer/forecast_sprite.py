d00809f9ed04450963f117364b9c753514b38415
nicholas-pfeifer <nicholas.pfeifer@ndsu.edu>
2019-10-12 20:29:18 -0500

    Visualizer displays forecast images

--- game/visualizer/forecast_sprite.py
+++ game/visualizer/forecast_sprite.py
@@ -7,20 +7,27 @@ class ForecastLayer(cocos.layer.Layer):
         self.turn = turn
         self.parser = log_parser
         super().__init__()
+        images = {
+            "0": "game/visualizer/assets/forecast_assets/tape_fire.png",
+            "1": "game/visualizer/assets/forecast_assets/tape_tornado.png",
+            "2": "game/visualizer/assets/forecast_assets/tape_hurricane.png",
+            "3": "game/visualizer/assets/forecast_assets/tape_earthquake.png",
+            "4": "game/visualizer/assets/forecast_assets/tape_monster.png",
+            "5": "game/visualizer/assets/forecast_assets/tape_ufo.png",
+            "6": "game/visualizer/assets/forecast_assets/tape_clear.png"
+        }
 
         forecast = self.parser.turns[clamp(turn-3, 0, turn):clamp(turn+2, 0, len(self.parser.turns)):]
 
         for i in range(len(forecast)):
-            text = str(forecast[i]["player"].get('city').get('sensors').get('0').get('sensor_results'))
-            label = cocos.text.Label('0' if text is ' ' else text,
-                                     font_name="Times New Roman",
-                                     font_size=64,
-                                     anchor_x='center',
-                                     anchor_y='center')
+            spr = cocos.sprite.Sprite(images["6"])
+            for key, item in forecast[i]['rates'].items():
+                if item == 0:
+                    spr = cocos.sprite.Sprite(images[key])
             if self.turn < 2:
-                label.position = (i+3)*192, 200
+                spr.position = (i+3)*64, 200
             elif self.turn < 3:
-                label.position = (i + 2) * 192, 200
+                spr.position = (i+2)*64, 200
             else:
-                label.position = (i+1)*192, 200
-            self.add(label)
+                spr.position = (i+1)*64, 200
+            self.add(spr)
