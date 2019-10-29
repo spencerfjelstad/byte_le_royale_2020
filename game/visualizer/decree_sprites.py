cfb40d9dc1b4706d126af4a448c4c37fed5d4aa5
nicholas-pfeifer <nicholas.pfeifer@ndsu.edu>
2019-10-29 15:25:33 -0500

    Created load function, replaced gif's with png's, and added worker sprites

--- game/visualizer/decree_sprites.py
+++ game/visualizer/decree_sprites.py
@@ -2,22 +2,14 @@ import cocos
 
 
 class DecreeLayer(cocos.layer.Layer):
-    def __init__(self, turn_info, display_size):
+    def __init__(self, turn_info, display_size,assets):
         self.display = display_size
         self.info = turn_info
+        self.images = assets
         super().__init__()
-        # Dictionary of images for the different decrees
-        images = {
-            "0":"game/visualizer/assets/decree_assets/decree_0.png",
-            "1":"game/visualizer/assets/decree_assets/decree_1.png",
-            "2":"game/visualizer/assets/decree_assets/decree_2.png",
-            "3":"game/visualizer/assets/decree_assets/decree_3.png",
-            "4":"game/visualizer/assets/decree_assets/decree_4.png",
-            "5":"game/visualizer/assets/decree_assets/decree_5.png"
-        }
 
         # Choose image for decree based on the turn info and add it to the layer
         image = str(self.info['player'].get('action').get('decree'))
-        spr = cocos.sprite.Sprite(images[image])
+        spr = self.images[image]
         spr.position = 50, self.display[1]-50
         self.add(spr)
