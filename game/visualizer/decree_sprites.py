9b855e7f7da5d683dd69b6375ddadc37881872c6
nicholas-pfeifer <nicholas.pfeifer@ndsu.edu>
2020-01-20 16:17:24 -0600

    Added visual feedback when decree matches disaster, fixed #178

--- game/visualizer/decree_sprites.py
+++ game/visualizer/decree_sprites.py
@@ -8,12 +8,23 @@ class DecreeLayer(cocos.layer.Layer):
         self.parser = log_parser
         self.images = assets
         super().__init__()
+
         if self.turn > 1:
             info = self.parser.get_turn(self.turn-1)
         else:
             info = self.parser.get_turn(self.turn)
+        current_turn = self.parser.get_turn(self.turn)
+        disaster = -1
+
         # Choose image for decree based on the turn info and add it to the layer
-        image = str(info['player'].get('action').get('decree'))
-        spr = self.images[image]
+        decree = str(info['player'].get('action').get('decree'))
+        spr = self.images[decree]
         spr.position = 50, self.display[1]-50
         self.add(spr)
+
+        success_label = cocos.text.Label("Protected!",font_name='Comic Sans',font_size=16,anchor_x='center',position=(50,self.display[1]-100))
+
+        if len(current_turn['events']) > 0:
+            disaster = current_turn['events'][0].get('disaster').get('disaster_type')
+        if int(decree) == disaster:
+            self.add(success_label)
