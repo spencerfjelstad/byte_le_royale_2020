b7669424c3b6b51006258ca1fb8afbf0f9b5bbfe
nicholas-pfeifer <nicholas.pfeifer@ndsu.edu>
2020-02-02 16:41:59 -0600

    Split sensors into two layers

--- game/visualizer/sensor_sprites.py
+++ game/visualizer/sensor_sprites.py
@@ -1,7 +1,7 @@
 import cocos
 
 
-class SensorLayer(cocos.layer.Layer):
+class BackSensorLayer(cocos.layer.Layer):
     def __init__(self, display_size, turn_info, assets):
         self.display = display_size
         self.info = turn_info
@@ -10,7 +10,7 @@ class SensorLayer(cocos.layer.Layer):
 
         for key, value in self.info["player"]["city"]["sensors"].items():
             sensor = self.images[key][str(value["level"])]
-            threat = self.images[key][self.determine_threat(value["sensor_results"])]
+            threat = self.images[key][determine_threat(value["sensor_results"])]
             key = int(key)
             if key == 0:
                 sensor.position = 1228, self.display[1]-260
@@ -20,7 +20,22 @@ class SensorLayer(cocos.layer.Layer):
                 sensor.position = 748, self.display[1]-164
             elif key == 3:
                 sensor.position = 220, self.display[1]-304
-            elif key == 4:
+            threat.position = sensor.position
+            self.add(sensor)
+            self.add(threat)
+
+class FrontSensorLayer(cocos.layer.Layer):
+    def __init__(self, display_size, turn_info, assets):
+        self.display = display_size
+        self.info = turn_info
+        self.images = assets
+        super().__init__()
+
+        for key, value in self.info["player"]["city"]["sensors"].items():
+            sensor = self.images[key][str(value["level"])]
+            threat = self.images[key][determine_threat(value["sensor_results"])]
+            key = int(key)
+            if key == 4:
                 sensor.position = 532, self.display[1]-300
             elif key == 5:
                 sensor.position = 44, self.display[1]-388
@@ -28,12 +43,12 @@ class SensorLayer(cocos.layer.Layer):
             self.add(sensor)
             self.add(threat)
 
-    def determine_threat(self, results):
-        if results == 0.0:
-            return "threat_3"
-        elif results <= 0.25:
-            return "threat_1"
-        elif results <= 0.5:
-            return "threat_2"
-        else:
-            return "threat_3"
+def determine_threat(results):
+    if results == 0.0:
+        return "threat_3"
+    elif results <= 0.25:
+        return "threat_1"
+    elif results <= 0.5:
+        return "threat_2"
+    else:
+        return "threat_3"
