3827bf73d1a6cf4a08fac2fae9c23ef706d026be
topoftheyear <topoftheyear@Gmail.com>
2020-02-02 19:26:20 -0600

    Reworks loading screen to use background image

--- game/visualizer/loading_layer.py
+++ game/visualizer/loading_layer.py
@@ -1,6 +1,6 @@
 import cocos
 
-from game.visualizer.load import load
+from game.visualizer.load import load, find_image
 
 
 class LoadingLayer(cocos.layer.Layer):
@@ -10,33 +10,39 @@ class LoadingLayer(cocos.layer.Layer):
         self.assets = assets
         self.post_method = post_method
 
+        loading_image = cocos.sprite.Sprite(
+            find_image('game/visualizer/assets/city_assets/loading_screen.png'),
+            (640, 360),
+        )
+        self.add(loading_image)
+
         self.loading_order = {
-            1: "Loading city assets",
-            2: "Loading side structures",
-            3: "Creating disasters",
-            4: "Initializing forecasts",
-            5: "Constructing sensors",
-            6: "Writing decrees",
-            7: "Importing workers",
-            8: "Cleaning up"
+            1: 'Loading city assets',
+            2: 'Loading side structures',
+            3: 'Creating disasters',
+            4: 'Initializing forecasts',
+            5: 'Constructing sensors',
+            6: 'Writing decrees',
+            7: 'Importing workers',
+            8: 'Cleaning up'
         }
 
         self.label = cocos.text.Label(
             '',
             font_name='Arial',
             font_size=16,
-            color=(255, 255, 255, 255),
+            color=(0, 125, 0, 255),
             anchor_x='center',
             anchor_y='center'
 
         )
-        self.label.position = 640, 360
+        self.label.position = 640, 50
         self.add(self.label)
 
         self.bar = cocos.draw.Line(
-            (540, 260),
-            (540, 260),
-            color=(0, 150, 255, 255),
+            (540, 30),
+            (540, 30),
+            color=(0, 125, 0, 255),
             stroke_width=16
         )
         self.add(self.bar)
@@ -51,7 +57,7 @@ class LoadingLayer(cocos.layer.Layer):
             item = self.loading_order[self.current_key]
 
             self.label.element.text = item
-            self.bar.end = (540 + (200 * self.current_key / len(self.loading_order)), 260)
+            self.bar.end = (540 + (200 * self.current_key / len(self.loading_order)), 30)
 
             load(self.assets, self.current_key)
         else:
