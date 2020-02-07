e4ff050275bfc8ca86e64d769e93b83794c1ffa2
Cinnamon Toast <topoftheyear@Gmail.com>
2020-02-06 19:42:11 -0600

    Unbroke tables

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -33,7 +33,7 @@ from recommonmark.transform import AutoStructify
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark', 'sphinx.ext.todo', 'sphinx.ext.githubpages']
+extensions = ['recommonmark', 'sphinx.ext.todo', 'sphinx.ext.githubpages', 'sphinx_markdown_tables']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
